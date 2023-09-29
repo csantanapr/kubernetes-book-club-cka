13a5b56c0469e8d23ad1fbfbf19df4228961fd41
Carlos Santana <csantana23@gmail.com>
2023-09-29 13:08:27 -0400

    add the rest of the files
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,16 @@
+mkdir -p $HOME/.kube
+kubectl completion bash > /home/vscode/.kube/completion.bash.inc
+printf "
+source /usr/share/bash-completion/bash_completion
+source $HOME/.kube/completion.bash.inc
+alias k=kubectl
+complete -F __start_kubectl k
+" >> $HOME/.bashrc
+
+printf "
+source <(kubectl completion zsh)
+alias k=kubectl
+complete -F __start_kubectl k
+" >> $HOME/.zshrc
+
+kind create cluster || true
